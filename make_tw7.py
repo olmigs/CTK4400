af40929b78908014cdf1fbf4ccb8ee9324b8b65b
michgz <mdr.nz11@gmail.com>
2022-12-20 22:03:56 +0000

    Works!

--- make_tw7.py
+++ make_tw7.py
@@ -50,14 +50,20 @@ B += b"\x00" * 0xFC
 B += b"\xDB\xD4\x50\x97\x69" + b"S2:Orgnl        "
 
 B += struct.pack("<2H", 0xEF, 0xE8)
-B += struct.pack("<4I", 0x2000, 0x2000, 0, 0)
-B += b"\x00"
+B += struct.pack("<3I", 0x20, 0x20, 0)
+B += b"\x00\x00\x00"
 
 B += struct.pack("<7I", 0x22, 0, 0x22, 0, 0x22, 0, 0x22)
 B += b"\x00\x00"
 
-B += struct.pack("<IIH", LEN - 0x28, 0x20000, LEN - 0x18)
-
+B += struct.pack("<II", LEN - 0x28, 0x20000)
+B += b"\x00"
+B += struct.pack("<H", LEN - 0x18)
+B += b"\x00"
+B += struct.pack("<H", LEN - 0x08)
+B += struct.pack("<IH", 0, 2)
+B += b"\xa1\x53"
+B += struct.pack("<IH", 0x80023C, 0)
 
 with wave.open(os.path.join("Wav", "S2.wav"), "rb") as f:
     print(f.getsampwidth())
@@ -80,8 +86,11 @@ Z = (numpy.frombuffer(C, dtype=numpy.uint8) - 128).astype(numpy.int8)   # zero-p
 TARGET_FREQ = 16000
 
 if Z_FRAMERATE == TARGET_FREQ:
-    W = Z
+    # Remove the last sample. I think the problem here is mainly the encoding was
+    # done wrong.
+    W = Z[:-1]
 else:
+    print("Resampling")
     U = scipy.signal.resample(Z, int( float(TARGET_FREQ) / float(f.getframerate()) * float(Z.size)  )  )
 
     print(U)
