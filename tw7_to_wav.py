bde2e6823057a64e9286cdceb16e157141d20ad5
michgz <mdr.nz11@gmail.com>
2022-12-20 22:14:44 +0000

    Also do the other direction

---
+++ tw7_to_wav.py
@@ -0,0 +1,40 @@
+"""
+Pull out a Wave from a TW7 file
+"""
+
+
+from matplotlib import pyplot as plt
+import numpy
+import wave
+import struct
+
+import pathlib
+
+
+
+
+def tw7_to_wav(INPUT : pathlib.Path, OUTPUT : pathlib.Path):
+
+    with open(INPUT, "rb") as f1:
+        X = f1.read()
+
+    Z = (numpy.frombuffer(X[0x176:], dtype=numpy.int8) + 128).astype(numpy.uint8)
+
+
+    f2 = wave.open(str(OUTPUT), "wb")
+    f2.setnchannels(1)
+    f2.setsampwidth(1)
+    f2.setframerate(16000)
+
+    f2.writeframes(bytes(Z))
+
+    f2.close()
+
+
+
+
+if __name__=="__main__":
+    
+    tw7_to_wav("S2_Orgnl.tw7", pathlib.Path("Wav", "S2.wav"))
+
+
